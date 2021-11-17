c1520a819bb5746de6fa3b392cbdf697e6b92601
Renukadeveloper96 <renukamaradagi@gmail.com>
2021-11-17 19:41:01 +0530

    finali

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use (bodyParser.urlencoded({extended:true}));
 app.use (bodyParser.json());
 app.use (cors());
 //const mongourl ="mongodb://localhost:27017"
-const mongourl ="mongodb+srv://zomato:zomato1277@cluster0.nbutl.mongodb.net/accessories?retryWrites=true&w=majority";
+const mongourl ="mongodb+srv://zomato:zomato127@cluster0.nbutl.mongodb.net/accessories?retryWrites=true&w=majority";
 var  db;  
 
 
@@ -22,10 +22,107 @@ app.get('/',(req,res) =>{
     res.send("welcome to accessories")
 })
 
-//list all accessories
-app.get('/all',(req,res)=>{
-    db.collection('all').find().toArray
+
+//list all quicksearches
+app.get('/quicksearch',(req,res)=>{
+    db.collection('main').find().toArray
     ((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//list all accessories
+/*app.get('/main',(req,res)=>{
+    db.collection('main').find().toArray
+    ((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})*/
+
+//list main wrt to product_id:query:http://localhost:8210/main?PRODUCTID=1
+/*app.get('/main',(req,res)=>{
+    var pro=req.query.pro?req.query.pro:"2";
+    db.collection('main').find({"type.Product_Id":pro}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})*/
+//list main wrt to product_id:query:http://localhost:8210/main?PRODUCTID=1
+/*app.get('/main',(req,res)=>{
+    var PRODUCTID=req.query.PRODUCTID
+    db.collection('main').find({product_id:PRODUCTID}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})*/
+
+
+//list main wrt to product_id:params:http://localhost:8210/main/1
+app.get('/main/:PRODUCTID',(req,res)=>{
+    var PRODUCTID=req.params.PRODUCTID
+    db.collection('main').find({product_id:PRODUCTID}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+http://localhost:8210/main/1?mealtype=3
+app.get('/main:PRODUCTID',(req,res)=>{
+    var query={}
+    if(req.query.PRODUCTID){
+        query={product_id:req.query.PRODUCTID}
+    }else if(req.query.mealtype){
+        query={"type.Product_Id":req.query.mealtype}
+    }
+    db.collection('main').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+http://localhost:8210/filter/1?STUDID=1
+app.get('/filter/:PRODUCTID',(req,res)=>{
+    var PRODUCTID=req.params.PRODUCTID;
+    var query={"type.Product_Id":PRODUCTID};
+    if(req.query.STUDID){
+        query={"type.Product_Id":PRODUCTID,"Subtype.sub_id":req.query.STUDID}
+    }
+    db.collection('all').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//restaurant with mealtype
+http://localhost:8210/all?TYPE=3
+app.get('/all',(req,res)=>{
+    var TYPE=req.query.TYPE?req.query.TYPE:3;
+    db.collection('all').find({"type.ProductId":TYPE}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+http://localhost:8210/all?TYPE=3
+app.get('/all',(req,res)=>{
+    var TYPE=req.query.TYPE?req.query.TYPE:3;
+    db.collection('all').find({"type.mealtype":TYPE}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+
+//query param:http://localhost:8210/all?cityId=7
+app.get('/all',(req,res)=>{
+    var cityId=req.query.cityId;
+    db.collection('all').find({productid:cityId}).toArray((err,result)=>{
         if(err) throw err;
         res.send(result)
     })
